4d1a3e12aef8c8c5883f5a36de5461138be94cc6
Kohei <kkimura62@icloud.com>
2016-05-06 14:41:59 +0900

    complete chapter2

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 	"sync"
 	"text/template"
 	"trace"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 type templateHandler struct {
@@ -22,11 +26,21 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ =
 			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse()
+	gomniauth.SetSecurityKey("nejimakidori0602")
+	gomniauth.WithProviders(
+		google.New("503457730410-4ds3fn9no7uuki41u043s707k2vsa0b7.apps.googleusercontent.com", "GTDbhxN3xnfSvIMN1uWiRSmk", "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
